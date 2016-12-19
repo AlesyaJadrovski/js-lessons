d921ee52d4ee041969cd0383372450c1173c2791
Roma Jadrovski <roma.jadrovski@live.com>
2016-12-19 22:01:37 +0300

    add obj

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert('Hello!');
+var num = 5;
+var str = 'hello';
+var und;
+var f  = function(){};
+var arr = [];
+var obj = {
+  name: 'Алеся',
+  display: function(){
+    alert(this.name);
+  }
+};
+obj.display();

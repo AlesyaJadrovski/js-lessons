47e13da66e5fd3b24449e78b016b3547e0672959
Roma Jadrovski <roma.jadrovski@live.com>
2016-12-26 22:16:30 +0300

    todo list, events,  part 1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-var num = 5;
-var str = 'hello';
-var und;
-var f  = function(){};
-var arr = [];
-var obj = {
-  name: 'Алеся',
-  display: function(){
-    alert(this.name);
+'use strict';
+
+var todoListElement = document.getElementById('todo-list');
+var buttonAddElement = document.getElementById('btn-add');
+var inputTodoElement = document.getElementById('todo-input');
+var addTodoHandler = function(event) {
+  if(event.type == 'keyup' && event.keyCode != 13) {
+    return;
   }
+  // todo work
 };
-obj.display();
+buttonAddElement.onclick = addTodoHandler;
+inputTodoElement.onkeyup = addTodoHandler;

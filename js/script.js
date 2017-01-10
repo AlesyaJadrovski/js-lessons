10ca4920a78fa88be2a8f2fcbea5b9f3294ecbb0
AlesyaJadrovski <alesya.smirnova@hotmail.com>
2017-01-10 16:53:37 +0300

    jquery

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 'use strict';
 
-var todoListElement = document.getElementById('todo-list');
-var formElement = document.getElementById('todo-controls-form');
-var inputTodoElement = document.getElementById('todo-input');
+var todoListElement = $("#todo-list");
+var formElement = $("#todo-controls-form");
+var inputTodoElement = $("#todo-input");
 var addTodoHandler = function(event) {
   event.preventDefault();
   if(inputTodoElement.value.trim() == '') {
     inputTodoElement.value = '';
     return;
   }
-  var newListItem = document.createElement('li');
-  var newTodoText = document.createTextNode(inputTodoElement.value);
-  newListItem.appendChild(newTodoText);
-  todoListElement.appendChild(newListItem);
+  var newListItem = '<li>' + inputTodoElement.val() + '</li>';
+  todoListElement.append(newListItem);
   inputTodoElement.value = '';
 };
 formElement.onsubmit = addTodoHandler;
+
